f0765a90ddf5b2795d8276de7f2ace76be475e88
ViniciusGregorio <112400890+ViniciusGregorio@users.noreply.github.com>
2025-12-24 15:41:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+const form = document.getElementById("formContato");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  msg.textContent = "Mensagem enviada com sucesso!";
+  msg.style.color = "green";
+  form.reset();
+});
